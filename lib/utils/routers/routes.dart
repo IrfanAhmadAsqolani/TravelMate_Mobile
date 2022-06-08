a2c8167ae82a2c201d7acc31b5d40d208473a261
IrfanAhmadAsqolani <imadas19@gmail.com>
2022-06-08 23:40:33 +0700

    finish slicing my invitation page and my invitation detail page

--- lib/utils/routers/routes.dart
+++ lib/utils/routers/routes.dart
@@ -8,6 +8,8 @@ import '../../features/destination_detail/pages/destination_detail_page.dart';
 import '../../features/invitation_detail/pages/invitation_detail_page.dart';
 import '../../features/joined_invitation/pages/joined_invitation_page.dart';
 import '../../features/login/pages/login_page.dart';
+import '../../features/my_invitation/pages/my_invitation_page.dart';
+import '../../features/my_invitation_detail/pages/my_invitation_detail_page.dart';
 import '../../features/register/pages/register_page.dart';
 
 class Routes {
@@ -31,6 +33,14 @@ class Routes {
       GetPage(
         name: joinedInvitationPageRoute,
         page: () => JoinedInvitationPage(),
+      ),
+      GetPage(
+        name: myInvitationPageRoute,
+        page: () =>MyInvitationPage(),
+      ),
+      GetPage(
+        name: myInvitationDetailPageRoute,
+        page: () =>MyInvitationDetailPage(),
       ),
     ];
   }
