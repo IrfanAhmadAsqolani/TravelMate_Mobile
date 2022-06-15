2fa7ed2e0b07a02d30d4275571ddca319313bb84
IrfanAhmadAsqolani <imadas19@gmail.com>
2022-06-15 22:33:57 +0700

    ketinggalan pak hehe

--- lib/components/custom_back_button.dart
+++ lib/components/custom_back_button.dart
@@ -15,7 +15,6 @@ class CustomBackButton extends StatelessWidget {
       child: InkWell(
         onTap: () {
           Get.back();
-          print("aaa");
         },
         child: SvgPicture.asset(
           Assets.icons.icChevronLeft,
