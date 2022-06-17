d0a6bc1431e6cbc3179f66d7be936c0dbbf7b48c
Aditya Andar Rahim <adityandar@gmail.com>
2022-06-17 22:42:55 +0700

    Add new constants and enums

---
+++ lib/constants/common_constant.dart
@@ -0,0 +1,6 @@
+class CommonConstant {
+  CommonConstant._();
+
+  static const String networkImageBaseUrl =
+      'http://travelmate.masuk.id/storage/app/public/';
+}
---
+++ lib/enums/verification_status.dart
@@ -0,0 +1,1 @@
+enum VerificationStatus { pending, rejected, verified }
