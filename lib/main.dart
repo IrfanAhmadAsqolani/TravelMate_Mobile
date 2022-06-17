06387b3f4fc7e760bdea11736826ce2f507b5393
Aditya Andar Rahim <adityandar@gmail.com>
2022-06-17 23:59:19 +0700

    Change text-size wont adapt

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travelmate/utils/utils.dart';
 
+import 'authentication/authentication.dart';
 import 'dependencies/dependencies.dart';
 
 void main() async {
@@ -11,16 +12,25 @@ void main() async {
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
+  final AuthenticationManager _authManager = Get.put(AuthenticationManager());
+
   @override
   Widget build(BuildContext context) {
     //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
     return ScreenUtilInit(
       designSize: const Size(360, 844),
-      minTextAdapt: true,
+      minTextAdapt: false,
       splitScreenMode: true,
       builder: (context, child) {
         return GetMaterialApp(
           debugShowCheckedModeBanner: false,
+          builder: (context, widget) {
+            ScreenUtil.init(context);
+            return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+              child: widget!,
+            );
+          },
           theme: ThemeData(
             fontFamily: 'SFPro',
           ),
