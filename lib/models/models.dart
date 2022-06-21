70854236d04d4212124516e3d8d2c08ccf63cdc9
Aditya Andar Rahim <adityandar@gmail.com>
2022-06-22 00:58:04 +0700

    Add maps model and modify DestinationMdl to match new requirement

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -2,6 +2,7 @@ export 'authentication/authentication.dart';
 export 'category/category.dart';
 export 'destinations/destinations.dart';
 export 'invitations/invitations.dart';
+export 'maps/maps.dart';
 export 'province/province.dart';
-export 'users/users.dart';
 export 'travel_buddy/travel_buddy.dart';
+export 'users/users.dart';
