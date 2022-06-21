d07879e7f931091c7487736d02b946febd587c3e
Aditya Andar Rahim <adityandar@gmail.com>
2022-06-21 22:49:14 +0700

    Add implementation for onboarding interest

--- lib/features/onboarding/pages/onboarding_page.dart
+++ lib/features/onboarding/pages/onboarding_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:travelmate/authentication/authentication_manager.dart';
 import 'package:travelmate/features/main_page/main_page.dart';
+import 'package:travelmate/features/onboarding/controllers/onboarding_controller.dart';
 
 import '../../../dependencies/dependencies.dart';
 import 'views/views.dart';
@@ -16,24 +18,44 @@ class OnboardingPage extends StatefulWidget {
 class _OnboardingPageState extends State<OnboardingPage> {
   int index = 0;
   PageController controller = PageController();
+
   @override
   Widget build(BuildContext context) {
+    Get.put(OnboardingController());
+
     return Scaffold(
       body: PageView(
         controller: controller,
         physics: const NeverScrollableScrollPhysics(),
         children: [
           OnboardingInterestView(
-            onContinueCallback: () => controller.jumpToPage(1),
+            onContinueCallback: () => onboardingNavigation(1),
           ),
           OnboardingUploadKTPView(
-            onContinueCallback: () => controller.jumpToPage(2),
+            onContinueCallback: () => onboardingNavigation(2),
           ),
           OnboardingCompletedView(
-            onContinueCallback: () => Get.offAllNamed(mainPageRoute),
+            onContinueCallback: () => onboardingNavigation(),
           ),
         ],
       ),
     );
+  }
+
+  Future<void> onboardingNavigation([int? newPage]) async {
+    if (newPage == null) {
+      Get.offAllNamed(mainPageRoute);
+      return;
+    }
+    final user = await Get.find<AuthenticationManager>().getMyUserData();
+    if (newPage == 1) {
+      if ((user?.identityPictUrl ?? '-') == '-') {
+        controller.jumpTo(1);
+      } else {
+        Get.offAllNamed(mainPageRoute);
+      }
+    } else if (newPage == 2) {
+      controller.jumpTo(2);
+    }
   }
 }
--- lib/features/onboarding/pages/widgets/interest_option_widget.dart
+++ lib/features/onboarding/pages/widgets/interest_option_widget.dart
@@ -1,42 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:travelmate/dependencies/dependencies.dart';
+import 'package:travelmate/features/onboarding/controllers/onboarding_controller.dart';
+import 'package:travelmate/models/models.dart';
 
 import '../../../../utils/utils.dart';
 
 class InterestOptionWidget extends StatelessWidget {
-  const InterestOptionWidget({
+  InterestOptionWidget({
     Key? key,
-    required this.title,
-    required this.onTap,
-    this.isActive = false,
+    required this.category,
   }) : super(key: key);
 
-  final String title;
-  final bool isActive;
-  final VoidCallback onTap;
+  final CategoryMdl category;
+
+  final OnboardingController onboardingController =
+      Get.find<OnboardingController>();
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        height: 30.w,
-        width: 1.sw,
-        margin: EdgeInsets.only(bottom: 14.w),
-        decoration: BoxDecoration(
-          color: (isActive) ? CustomColors.success : CustomColors.white,
-          borderRadius: BorderRadius.circular(30.r),
-          boxShadow: Shadows.defaultShadow(),
-        ),
-        child: Center(
-          child: Text(
-            title,
-            style: const TextStyle(
-              fontSize: 17,
+    return Obx(() {
+      return InkWell(
+        onTap: () => onboardingController.selectInterest(category),
+        child: Container(
+          height: 30.w,
+          width: 1.sw,
+          margin: EdgeInsets.only(bottom: 14.w),
+          decoration: BoxDecoration(
+            color: (onboardingController.selectedInterests.contains(category))
+                ? CustomColors.success
+                : CustomColors.white,
+            borderRadius: BorderRadius.circular(30.r),
+            boxShadow: Shadows.defaultShadow(),
+          ),
+          child: Center(
+            child: Text(
+              category.name ?? '',
+              style: const TextStyle(
+                fontSize: 17,
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
