61be02e641573a4023ebf59bf41eecade0611ead
IrfanAhmadAsqolani <imadas19@gmail.com>
2022-06-25 00:43:08 +0700

    integrate data in my invitation page

---
+++ lib/components/my_invitation_card.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:travelmate/components/components.dart';
+import 'package:travelmate/dependencies/dependencies.dart';
+import 'package:travelmate/features/invitation_detail/pages/invitation_detail_page.dart';
+import 'package:travelmate/features/my_invitation_detail/pages/my_invitation_detail_page.dart';
+import 'package:travelmate/utils/utils.dart';
+
+import '../helpers/helpers.dart';
+import '../models/models.dart';
+
+class MyInvitationCard extends StatelessWidget {
+  const MyInvitationCard({
+    Key? key,
+    required this.invitation,
+  }) : super(key: key);
+
+  final InvitationMdl invitation;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => Get.toNamed(
+        // invitationDetailPageRoute,
+        myInvitationDetailPageRoute,
+        arguments: invitation,
+      ),
+      child: Container(
+        width: 1.sw,
+        padding: EdgeInsets.fromLTRB(24.w, 11.w, 0.w, 11.w),
+        margin: EdgeInsets.symmetric(horizontal: 16.w),
+        decoration: BoxDecoration(
+          color: CustomColors.white,
+          borderRadius: BorderRadius.circular(10.r),
+          boxShadow: Shadows.defaultShadow(),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    invitation.title ?? '',
+                    style: TextStyles.heading5SemiBold(),
+                  ),
+                  SizedBox(height: 5.w),
+                  Text(
+                    DateHelper.getReadableDate(invitation.departDate),
+                    style: TextStyles.heading7Regular(),
+                  ),
+                  SizedBox(height: 5.w),
+                  Text(
+                    'Created by ${invitation.owner?.username ?? ''}',
+                    style: TextStyles.heading7Regular(
+                      color: CustomColors.grey,
+                    ),
+                  ),
+                  SizedBox(height: 5.w),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      StatusChip(isOpen: invitation.isOpen),
+                      SizedBox(width: 9.w),
+                      Text(
+                        '${invitation.maxTeam} Persons',
+                        style: TextStyles.heading7Regular(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 8.w),
+            const Icon(Icons.chevron_right_rounded),
+            SizedBox(width: 24.w),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/my_invitation_detail/pages/my_invitation_detail_page.dart
+++ lib/features/my_invitation_detail/pages/my_invitation_detail_page.dart
@@ -3,14 +3,22 @@ import 'package:travelmate/dependencies/dependencies.dart';
 
 import '../../../components/components.dart';
 import '../../../gen/assets.gen.dart';
+import '../../../helpers/helpers.dart';
 import '../../../models/models.dart';
 import '../../../utils/utils.dart';
 import '../../invitation_edit/pages/invitation_edit_page.dart';
 
 const String myInvitationDetailPageRoute = '/my-invitation-detail';
 
-class MyInvitationDetailPage extends StatelessWidget {
+class MyInvitationDetailPage extends StatefulWidget {
   const MyInvitationDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyInvitationDetailPage> createState() => _MyInvitationDetailPageState();
+}
+
+class _MyInvitationDetailPageState extends State<MyInvitationDetailPage> {
+  final invitation = Get.arguments as InvitationMdl?;
 
   @override
   Widget build(BuildContext context) {
@@ -28,17 +36,17 @@ class MyInvitationDetailPage extends StatelessWidget {
                 children: [
                   SizedBox(height: 26.w),
                   Text(
-                    'Pantai Pangandaran',
+                    '${invitation?.destination?.name}',
                     style: TextStyles.heading7Regular(),
                   ),
                   SizedBox(height: 10.w),
                   Text(
-                    'Touring Santai',
+                    invitation?.title ?? '',
                     style: TextStyles.heading4(),
                   ),
                   SizedBox(height: 10.w),
                   Text(
-                    '24 Juli 2022',
+                    DateHelper.getReadableDate(invitation?.createdAt),
                     style: TextStyles.heading5Regular(),
                   ),
                   SizedBox(height: 10.w),
@@ -48,27 +56,28 @@ class MyInvitationDetailPage extends StatelessWidget {
                       StatusChip(isOpen: true),
                       SizedBox(width: 10.w),
                       Text(
-                        '10 persons',
+                        '${invitation?.maxTeam} persons',
                         style: TextStyles.heading5Regular(),
                       ),
                       SizedBox(width: 10.w),
                       Text(
-                        '3 available slot',
+                        '3 available slot', // TODO(adityandar): fix this
                         style: TextStyles.heading7Regular(color: Colors.blue),
                       ),
                     ],
                   ),
                   SizedBox(height: 10.w),
                   Text(
-                    'Jadi inget nasehat alm nenek waktu abang gw lagi nikahan, dia nasihatin pas gw lagi duduk di samping sound sistem dan sampe sekarang gw gatau apa nasihatnya karna ga kedengeran Intinya ayo kita touring, prefer dom bandung. untuk budget palingan 500rb udah cukup',
+                    invitation?.description ?? '',
                     style: TextStyles.heading5Regular(),
                   ),
                   SizedBox(height: 10.w),
-                  const SmallProfileCard(
-                    user: UserMdl(
-                      fullName: 'You',
-                      pictUrl: 'https://via.placeholder.com/150',
-                    ),
+                  SmallProfileCard(
+                    user: invitation?.owner,
+                    // user: UserMdl(
+                    //   fullName: 'You',
+                    //   pictUrl: 'https://via.placeholder.com/150',
+                    // ),
                   ),
                   SizedBox(height: 10.w),
                   CustomOutlinedButton.info(
